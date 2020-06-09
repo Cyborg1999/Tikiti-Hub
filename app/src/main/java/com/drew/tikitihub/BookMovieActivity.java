d1c0c47af443a9fb25e6e7ab7868f9ce1fb86212
mark-mokaya <mark.mokaya@strathmore.edu>
2020-06-09 20:23:42 +0300

    Majority of the design

---
+++ app/src/main/java/com/drew/tikitihub/BookMovieActivity.java
@@ -0,0 +1,14 @@
+package com.drew.tikitihub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BookMovieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_movie);
+    }
+}
