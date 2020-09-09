db79046f3d5209e3964ba488fdd5d554f20b8361
cyborg1999 <andrewk.tonui@gmail.com>
2020-09-10 00:42:39 +0300

    Added Functionality to the Login and Register Pages

--- app/src/main/java/com/drew/tikitihub/RegisterActivity.java
+++ app/src/main/java/com/drew/tikitihub/RegisterActivity.java
@@ -2,13 +2,166 @@ package com.drew.tikitihub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Request.Method;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.drew.tikitihub.app.AppConfig;
+import com.drew.tikitihub.app.AppController;
+import com.drew.tikitihub.helper.SQLiteHandler;
+import com.drew.tikitihub.helper.SessionManager;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class RegisterActivity extends AppCompatActivity {
+    private static final String TAG = RegisterActivity.class.getSimpleName();
+    private Button btnRegister;
+    private EditText inputFullName;
+    private EditText inputEmail;
+    private EditText inputPassword;
+    private EditText inputConfrimPass;
+    private ProgressDialog progressDialog;
+    private SessionManager session;
+    private SQLiteHandler db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        inputFullName = findViewById(R.id.regName);
+        inputEmail =findViewById(R.id.regMail);
+        inputPassword = findViewById(R.id.regPass);
+        inputConfrimPass = findViewById(R.id.confrimPass);
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setCancelable(false);
+
+        session = new SessionManager(getApplicationContext());
+
+        db = new SQLiteHandler(getApplicationContext());
+
+        if (session.isLoggedIn()) {
+            // User is already logged in. Take him to main activity
+            Intent intent = new Intent(RegisterActivity.this,
+                    MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = inputFullName.getText().toString();
+                String email = inputEmail.getText().toString();
+                String password = inputPassword.getText().toString();
+                String confirmPass = inputConfrimPass.getText().toString();
+
+                if (password == confirmPass){
+                    if (!name.isEmpty() && !email.isEmpty() && !password.isEmpty()){
+                        registerUser(name,email,password);
+                    }else {
+                        Toast.makeText(getApplicationContext(),"Please enter your details!",Toast.LENGTH_LONG).show();
+                    }
+                }else{
+                    Toast.makeText(getApplicationContext(),"Incorrect password",Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
+
+
+    }
+
+    private void registerUser(final String name, final String email, final String password) {
+        String tag_string_req = "req_register";
+
+        progressDialog.setMessage("Registering ...");
+        showDialog();
+
+        StringRequest strReq = new StringRequest(Method.POST, AppConfig.URL_REGISTER, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, "Register Response: " + response.toString());
+                hideDialog();
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    boolean error = jsonObject.getBoolean("error");
+                    if (!error) {
+                        String uid = jsonObject.getString("uid");
+
+                        JSONObject user = jsonObject.getJSONObject("user");
+                        String name = user.getString("name");
+                        String email = user.getString("email");
+                        String created_at = user.getString("created_at");
+
+                        db.addUser(name, email, uid, created_at);
+
+                        Toast.makeText(getApplicationContext(), "User successfully registered. Try login now!", Toast.LENGTH_LONG).show();
+
+                        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                        startActivity(intent);
+                        finish();
+
+                    } else {
+                        String errorMsg = jsonObject.getString("error_msg");
+                        Toast.makeText(getApplicationContext(),
+                                errorMsg, Toast.LENGTH_LONG).show();
+
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Registration Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_LONG).show();
+                hideDialog();
+            }
+        }){
+
+            @Override
+            protected Map<String, String> getParams()  {
+
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("name",name);
+                params.put("email",email);
+                params.put("password",password);
+
+                return params;
+            }
+        };
+
+        AppController.getInstance().addToRequestQueue(strReq,tag_string_req);
+
+    }
+
+    private void hideDialog() {
+        if (!progressDialog.isShowing())
+            progressDialog.show();
+    }
+
+    private void showDialog() {
+        if (progressDialog.isShowing())
+            progressDialog.dismiss();
     }
 }
